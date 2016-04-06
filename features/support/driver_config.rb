4f25cff12f47a2fb1f42513eb8ea58e4cd7f5f94
Ganesh Khakare <ganesh.e.khakare@gmail.com>
2016-04-06 09:21:25 +0000

    driver_config.rb edited online with Bitbucket

--- features/support/driver_config.rb
+++ features/support/driver_config.rb
@@ -44,9 +44,17 @@ module URLTest
   def launch_driver_opera
     service = Selenium::WebDriver::Chrome::Service.new("/usr/bin/operadriver",48923)
     service.start
-    @driver = Selenium::WebDriver.for :remote,:url => service.uri,  :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.opera('opera.binary' => "/usr/bin/opera",'operaOptions'=> {'binary' => '/usr/bin/opera'} )
-    @driver.manage.timeouts.implicit_wait = 60
-    @driver.manage.window.maximize
+    sleep(10)
+    begin
+      @driver = Selenium::WebDriver.for :remote,:url => service.uri,  :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.opera('opera.binary' => "/usr/bin/opera",'operaOptions'=> {'binary' => '/usr/bin/opera'} )
+      @driver.manage.timeouts.implicit_wait = 60
+      @driver.manage.window.maximize
+    rescue
+      @driver.quit
+      @driver = Selenium::WebDriver.for :remote,:url => service.uri,  :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.opera('opera.binary' => "/usr/bin/opera",'operaOptions'=> {'binary' => '/usr/bin/opera'} )
+      @driver.manage.timeouts.implicit_wait = 60
+      @driver.manage.window.maximize
+    end
    
   end
 
