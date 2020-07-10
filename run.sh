88bf7e43aff4d03f2e56de5946ec1a0c5d21791a
aishwarya-mohite <aishwarya.mohite@reancloud.com>
2020-07-10 12:50:51 +0530

    reports changes

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# unzip vendor.zip
-# if [[ ! -e .bundle ]]; then
-#             mkdir .bundle
-# fi
-
- bundle install && bundle exec cucumber features/urlcrawl.feature -p selenium --format pretty --format json --out=report/features_report.json --format Formatter::HtmlFormatter --out=report/features_report.html
+ bundle install && bundle exec cucumber features/urlcrawl.feature -p selenium --format pretty --format json --out=report/features_report.json --format Formatter::HtmlFormatter --out=report/feature-overview.html
 
